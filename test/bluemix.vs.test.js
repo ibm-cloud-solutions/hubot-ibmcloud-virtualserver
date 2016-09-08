ff8c0063ca6acf30a05294f5df25e2e0d2a60937
Reinaldo Cruz <rcruz@us.ibm.com>
2016-09-08 16:00:30 -0400

    Fix tests with portend

--- test/bluemix.vs.test.js
+++ test/bluemix.vs.test.js
@@ -10,7 +10,7 @@ const Helper = require('hubot-test-helper');
 const helper = new Helper('../src/scripts');
 const expect = require('chai').expect;
 const mockUtils = require('./mock.utils.vs.js');
-const sprinkles = require('mocha-sprinkles');
+const portend = require('portend');
 
 // --------------------------------------------------------------
 // i18n (internationalization)
@@ -29,31 +29,6 @@ const i18n = new (require('i18n-2'))({
 // At some point we need to toggle this setting based on some user input.
 i18n.setLocale('en');
 
-// Length of time to wait for a message
-const timeout = 10000;
-
-// Return a promise that will be resolved in the specified # of ms.
-function delay(ms) {
-	return new Promise((resolve) => {
-		setTimeout(function(){
-			resolve();
-		}, ms);
-	});
-}
-
-function waitForMessageQueue(room, len){
-	return sprinkles.eventually({
-		timeout: timeout
-	}, function() {
-		if (room.messages.length < len) {
-			throw new Error('too soon');
-		}
-	}).then(() => false).catch(() => true).then((success) => {
-		// Great.  Move on to tests
-		expect(room.messages.length).to.eql(len);
-	});
-}
-
 // Passing arrow functions to mocha is discouraged: https://mochajs.org/#arrow-functions
 // return promises from mocha tests rather than calling done() - http://tobyho.com/2015/12/16/mocha-with-promises/
 describe('Interacting with Bluemix via Slack', function() {
@@ -65,172 +40,168 @@ describe('Interacting with Bluemix via Slack', function() {
 
 	beforeEach(function() {
 		room = helper.createRoom();
-		// Force all emits into a reply.
-		room.robot.on('ibmcloud.formatter', function(event) {
-			if (event.message) {
-				event.response.reply(event.message);
-			}
-			else {
-				event.response.send({attachments: event.attachments});
-			}
-		});
 	});
 
 	afterEach(function() {
 		room.destroy();
 	});
 
-	context('user calls `virtual server show`', function() {
-		it('should send a slack event with a list of virtual servers', function(done) {
-			room.robot.on('ibmcloud.formatter', function(event) {
-				expect(event.attachments.length).to.eql(2);
-				expect(event.attachments[0].title).to.eql('new-server-test');
-				expect(event.attachments[1].title).to.eql('new-server-test2');
-				done();
+	context('user calls `virtual server help`', function() {
+		it('should respond with help', function() {
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[0].message).to.contain(i18n.__('help.vs.destroy'));
+				expect(events[0].message).to.contain(i18n.__('help.vs.show'));
+				expect(events[0].message).to.contain(i18n.__('help.vs.reboot'));
+				expect(events[0].message).to.contain(i18n.__('help.vs.start'));
+				expect(events[0].message).to.contain(i18n.__('help.vs.stop'));
 			});
-			room.user.say('mimiron', '@hubot virtual server show').then();
+
+			room.user.say('mimiron', '@hubot virtual server help');
+			return p;
+		});
+	});
+
+	context('user calls `virtual server show`', function() {
+		it('should send a slack event with a list of virtual servers', function() {
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].attachments.length).to.eql(2);
+				expect(events[0].attachments[0].title).to.eql('new-server-test');
+				expect(events[0].attachments[1].title).to.eql('new-server-test2');
+			});
+
+			room.user.say('mimiron', '@hubot virtual server show');
+			return p;
 		});
 	});
 
 	context('user calls `virtual server start`', function() {
 		it('should respond with started', function() {
-			return room.user.say('mimiron', '@hubot virtual server start new-server-test2').then(() => {
-				return delay(100);
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.start.success', 'new-server-test2')]);
+			let p = portend.twice(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[1].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.start.in.progress', 'new-server-test2'));
+				expect(events[1].message).to.be.eql(i18n.__('vs.start.success', 'new-server-test2'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server start new-server-test2');
+			return p;
 		});
 
 		it('should respond with not found', function() {
-			return room.user.say('mimiron', '@hubot virtual server start new-server-test1').then(() => {
-				return delay(100);
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.name.not.found', 'new-server-test1')]);
+			let p = portend.twice(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[1].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.start.in.progress', 'new-server-test1'));
+				expect(events[1].message).to.be.eql(i18n.__('vs.name.not.found', 'new-server-test1'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server start new-server-test1');
+			return p;
 		});
 	});
 
 	context('user calls ` virtual server stop`', function() {
-		it('should respond with stopped', function(done) {
-			return room.user.say('mimiron', '@hubot virtual server stop new-server-test').then(() => {
-				return delay(100);
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.stop.confirm', 'new-server-test')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				setTimeout(() => {
-					let response = room.messages[room.messages.length - 2];
-					expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.stop.in.progress', 'new-server-test')]);
-					response = room.messages[room.messages.length - 1];
-					expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.stop.success', 'new-server-test')]);
-					done();
-				}, 500);
+		it('should respond with stopped', function() {
+			let p = portend.twice(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[1].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.stop.in.progress', 'new-server-test'));
+				expect(events[1].message).to.be.eql(i18n.__('vs.stop.success', 'new-server-test'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server stop new-server-test').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 
 		it('should respond with not found', function() {
-			return room.user.say('mimiron', '@hubot virtual server stop new-server-test1').then(() => {
-				return delay(100);
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.stop.confirm', 'new-server-test1')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.stop.in.progress', 'new-server-test1')]);
+			let p = portend.twice(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[1].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.stop.in.progress', 'new-server-test1'));
+				expect(events[1].message).to.be.eql(i18n.__('vs.name.not.found', 'new-server-test1'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server stop new-server-test1').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 	});
 
 	context('user calls `virtual server reboot`', function() {
 		it('should respond with rebooted', function() {
-			return room.user.say('mimiron', '@hubot virtual server reboot new-server-test').then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.reboot.confirm', 'new-server-test')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql([ 'mimiron', '@hubot yes' ]);
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.reboot.success', 'new-server-test'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server reboot new-server-test').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 
 		it('should respond with safe', function() {
-			return room.user.say('mimiron', '@hubot virtual server reboot new-server-test').then(() => {
+			room.user.say('mimiron', '@hubot virtual server reboot new-server-test').then(() => {
 				let response = room.messages[room.messages.length - 1];
 				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.reboot.confirm', 'new-server-test')]);
-				return room.user.say('mimiron', '@hubot no');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('general.safe.this.time', 'new-server-test')]);
+				room.user.say('mimiron', '@hubot no').then(() => {
+					let response = room.messages[room.messages.length - 1];
+					expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('general.safe.this.time', 'new-server-test')]);
+				});
 			});
 		});
 
 		it('should respond with not found', function() {
-			return room.user.say('mimiron', '@hubot virtual server reboot new-server-test-notfound').then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.reboot.confirm', 'new-server-test-notfound')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.name.not.found', 'new-server-test-notfound')]);
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.name.not.found', 'new-server-test1'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server reboot new-server-test1').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 	});
 
 	context('user calls `virtual server destroy`', function() {
-		it('should respond with destroyed', function(done) {
-			return room.user.say('mimiron', '@hubot virtual server destroy new-server-test').then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.destroy.confirm', 'new-server-test')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				return waitForMessageQueue(room, 4);
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.destroy.success', 'new-server-test')]);
-				done();
+		it('should respond with destroyed', function() {
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.destroy.success', 'new-server-test'));
 			});
+
+			room.user.say('mimiron', '@hubot virtual server destroy new-server-test').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 
 		it('should respond with safe', function() {
-			return room.user.say('mimiron', '@hubot virtual server destroy new-server-test').then(() => {
+			room.user.say('mimiron', '@hubot virtual server destroy new-server-test').then(() => {
 				let response = room.messages[room.messages.length - 1];
 				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.destroy.confirm', 'new-server-test')]);
-				return room.user.say('mimiron', '@hubot no');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('general.safe.this.time', 'new-server-test')]);
+				room.user.say('mimiron', '@hubot no').then(() => {
+					let response = room.messages[room.messages.length - 1];
+					expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('general.safe.this.time', 'new-server-test')]);
+				});
 			});
 		});
 
 		it('should respond with not found', function() {
-			return room.user.say('mimiron', '@hubot virtual server destroy new-server-test1').then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.destroy.confirm', 'new-server-test1')]);
-				return room.user.say('mimiron', '@hubot yes');
-			}).then(() => {
-				let response = room.messages[room.messages.length - 1];
-				expect(response).to.eql(['hubot', '@mimiron ' + i18n.__('vs.name.not.found', 'new-server-test1')]);
+			let p = portend.once(room.robot, 'ibmcloud.formatter').then(events => {
+				expect(events[0].message).to.be.a('string');
+				expect(events[0].message).to.be.eql(i18n.__('vs.name.not.found', 'new-server-test1'));
 			});
-		});
-	});
 
-	context('user calls `virtual server help`', function() {
-		beforeEach(function() {
-			return room.user.say('mimiron', '@hubot virtual server help');
-		});
-
-		it('should respond with help', function() {
-			expect(room.messages.length).to.eql(2);
-			let help = 'hubot virtual server delete|destroy|remove [virtualserver] - ' + i18n.__('help.vs.destroy') + '\n'
-				+ 'hubot virtual server show|list - ' + i18n.__('help.vs.show') + '\n'
-				+ 'hubot virtual server reboot [virtualserver] - ' + i18n.__('help.vs.reboot') + '\n'
-				+ 'hubot virtual server start [virtualserver] - ' + i18n.__('help.vs.start') + '\n'
-				+ 'hubot virtual server stop [virtualserver] - ' + i18n.__('help.vs.stop') + '\n';
-			expect(room.messages[1]).to.eql(['hubot', '@mimiron \n' + help]);
+			room.user.say('mimiron', '@hubot virtual server destroy new-server-test1').then(() => {
+				room.user.say('mimiron', '@hubot yes');
+			});
+			return p;
 		});
 	});
 });
